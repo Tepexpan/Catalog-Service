c4b0a6b979c48a4fc303ce1d968ba18e78c58c68
Antonio Valdes Romero <tepe.skapara@gmail.com>
2025-04-15 19:51:58 -0600

    --CHANGE: se crea integracion con flyway
    Se crean los test con testcontainers

--- src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -1,14 +1,22 @@
 package com.polarbookshop.catalogservice.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 
-public interface BookRepository {
-    Iterable<Book> findAll();
+public interface BookRepository  extends CrudRepository<Book, Long> {
+
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
-    Book save(Book book);
-    void deleteByIsbn(String isbn);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM book WHERE isbn = :isbn")
+    void deleteByIsbn(@Param("isbn") String isbn);
 }
